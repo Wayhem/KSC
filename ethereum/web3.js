4b0d546107d2e5a31c16a6c5387fcf059c0bdd1e
Jose Villarroel <oscarclarke9900@gmail.com>
2018-11-27 15:00:51 -0400

    Fixed web3 to work with Infura or metamask provider in case it exists, also starting to do index page, semantic-ui

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,17 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    //metamask has inyected web3 in the browser
+    web3 = new Web3(window.web3.currentProvider);
+    //not handling 2nd november metamask update   
+} else {
+    //not on the browser or user does not use metamask
+    const provider = new Web3.providers.HttpProvider(
+        process.env.INFURAKEY
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
