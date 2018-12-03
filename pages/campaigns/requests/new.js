46e7e3f7b3467ea1c7f83a7420969c0df9054e00
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-02 20:51:55 -0400

    Added form for new request, has a little bug that spawns one in a thounsand times, need heavy testing to check whats wrong.

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,21 +1,106 @@
 import React, { Component } from 'react';
-import { Form, Button, Message } from 'semantic-ui-react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
 import Campaign from '../../../ethereum/campaign';
 import web3 from '../../../ethereum/web3';
 import { Link, Router } from '../../../routes';
+import withAuth from '../../../utils/withAuth';
+import swal from 'sweetalert2';
+import Layout from '../../../components/layout';
 
 class RequestNew extends Component {
-  static async getInitialProps(props) {
-    const { address } = props.query;
+    state = {
+        manager: false,
+        value: '',
+        description: '',
+        recipient: '',
+        loading: false,
+        errorMessage: ''
+    }
 
-    return { address };
-  }
+    componentDidMount() {
+        console.log(this.props);
+        const profile = this.props.auth.getProfile();
+        console.log(profile);
+        //BUG PROBLEM sometimes says profile.campaign is undefined so can't map it
+        var result = profile.campaign.map(campaign => ({ address: campaign.address }));
+        result.forEach((result) => {
+            if (this.props.url.query.address == result.address){
+                this.setState({ manager: true });
+            }
+        });
+        setTimeout(() => {
+            if (!this.state.manager){
+                swal({
+                    type: 'warning',
+                    title: 'Sorry!',
+                    text: 'You need to be the manager of the campaign to do that!',
+                    footer: '<a href="/home">Go to home page.</a>'
+                })
+                setTimeout(() => {
+                    window.location.replace('/home');
+                }, 3000)
+            }
+        }, 1000)
+    }
 
-  render() {
+    onSubmit = async e => {
+        e.preventDefault();
+        
+        const campaign = Campaign(this.props.url.query.address);
+        const { description, value, recipient } = this.state;
+
+        this.setState({ loading: true, errorMessage: '' })
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods
+                .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
+                .send({ from: accounts[0] });
+            Router.pushRoute(`/campaigns/${this.props.url.query.address}/requests`); 
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({ loading: false });
+    }
+
+    render() {
     return (
-        <h3>im gay</h3>
+        <Layout>
+            <Link route={`/campaigns/${this.props.url.query.address}/requests`}>
+                <a>
+                    Back
+                </a>
+            </Link>
+            <h3>Make a Request</h3>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+                <Form.Field>
+                    <label>Description</label>
+                    <Input 
+                        value={this.state.description}
+                        onChange={event => this.setState({ description: event.target.value })}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Value in Ether</label>
+                    <Input
+                        value={this.state.value}
+                        onChange={event => this.setState({ value: event.target.value })} 
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Recipient</label>
+                    <Input
+                        value={this.state.recipient}
+                        onChange={event => this.setState({ recipient: event.target.value })} 
+                    />
+                </Form.Field>
+                <Message error header="Oops!" content={this.state.errorMessage} />
+                <Button loading={this.state.loading} primary>Request!</Button>
+            </Form>
+        </Layout>
     );
   }
 }
 
-export default RequestNew;
+export default withAuth(RequestNew);
