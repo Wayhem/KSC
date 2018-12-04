2d05b7120c229f5c0cd8e41da15efd7e4f9c48fb
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-03 20:27:24 -0400

    Finalize request now possible, fixed ui/ux details on request index, the project is basically on alpha and ready for the 1st deployment, however there are several things to do with the project, Add an adequate README, redeploy new contract for production site, DRY the code a bit and refactor some functions, find the source of some bugs and fix them completely (temporary patch to the object/array being returned from getProfile()), style, page is ALL white and could use some styling (even for a dashboard it is really white), also very important refactor the contract so it can contain a title and a description, would be a nice feature

--- components/requestRow.js
+++ components/requestRow.js
@@ -5,15 +5,92 @@ import AuthService from '../utils/AuthService';
 import Domain from 'domain';
 import Campaign from '../ethereum/campaign';
 import swal from 'sweetalert2';
+const Auth = new AuthService(Domain);
 
 class RequestRow extends Component {
 
     state = {
-        contributor: false
+        contributor: false,
+        manager: false
+    }
+
+    onFinalize = async () => {
+        const Denmark = await Auth.loggedIn();
+        if (Denmark){    
+            const profile = Auth.getProfile();
+            if(!profile.length){
+                var result = profile.campaign.map(campaign => ({ address: campaign.address }));
+                result.forEach((result) => {
+                    if (this.props.address == result.address){
+                        this.setState({ manager: true });
+                    }
+                });
+                setTimeout(() => {
+                    if (!this.state.manager){
+                        swal({
+                            type: 'warning',
+                            title: 'Sorry!',
+                            text: 'You need to own the campaign to do that!',
+                            footer: '<a href="/home">Go to home page.</a>'
+                        })
+                        setTimeout(() => {
+                            window.location.replace(`/campaigns/${this.props.address}`);
+                        }, 3000)
+                    }
+                }, 1000)
+                if (this.state.manager) {
+                    const campaign = new Campaign(this.props.address);
+                    const accounts = await web3.eth.getAccounts();
+                    await campaign.methods.finalizeRequest(this.props.id).send({
+                        from: accounts[0]
+                    });
+                    this.setState({ manager: false });
+                    window.location.replace(`/campaigns/${this.props.address}/requests`);
+                }
+            } else if (profile.length){
+                var result = profile[0].campaign.map(campaign => ({ address: campaign.address }));
+                result.forEach((result) => {
+                    if (this.props.address == result.address){
+                        this.setState({ manager: true });
+                    }
+                });
+                setTimeout(() => {
+                    if (!this.state.manager){
+                        swal({
+                            type: 'warning',
+                            title: 'Sorry!',
+                            text: 'You need to own the campaign to do that!',
+                            footer: '<a href="/home">Go to home page.</a>'
+                        })
+                        setTimeout(() => {
+                            window.location.replace(`/campaigns/${this.props.address}`);
+                        }, 3000)
+                    }
+                }, 1000)
+                if (this.state.manager) {
+                    const campaign = new Campaign(this.props.address);
+                    const accounts = await web3.eth.getAccounts();
+                    await campaign.methods.finalizeRequest(this.props.id).send({
+                        from: accounts[0]
+                    });
+                    this.setState({ manager: false });
+                    window.location.replace(`/campaigns/${this.props.address}/requests`);
+                }
+            }
+        } else {
+            swal({
+                type: 'warning',
+                title: 'Sorry!',
+                text: 'You need to be logged in to do that!',
+                footer: '<a href="/register">New user? click here to register.</a>'
+            })
+            setTimeout(() => {
+                window.location.replace('/login');
+            }, 2500)
+        }
     }
 
     onApprove = async () => {
-        const Auth = new AuthService(Domain);
         const Denmark = await Auth.loggedIn();
         //sometimes doesn't return loggedin in time
         if (Denmark){    
@@ -34,7 +111,6 @@ class RequestRow extends Component {
                             footer: '<a href="/home">Go to home page.</a>'
                         })
                         setTimeout(() => {
-                            console.log('nigger');
                             window.location.replace(`/campaigns/${this.props.address}`);
                         }, 3000)
                     }
@@ -94,17 +170,25 @@ class RequestRow extends Component {
     render () {
         const { Row, Cell } = Table;
         const { id, request, approversCount } = this.props;
+        const readyFinal = request.yesCount > approversCount / 2;
+
         return(
-            <Row>
+            <Row disabled={request.complete} positive={readyFinal && !request.complete}>
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.yesCount}/{approversCount}</Cell>
                 <Cell>
+                    {request.complete ? null : (
                     <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+                    )}
                 </Cell>
-                <Cell></Cell>
+                <Cell>
+                    {request.complete ? null : (
+                    <Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+                    )}
+                </Cell>
             </Row>
         );
     }
