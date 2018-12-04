4a07fdb11bfe2c5e77154701aeac9e9b4b94f62d
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-04 14:27:42 -0400

    added factory address as a enviromental variable, soon to add README with detailed info

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import Factory from './build/Factory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(Factory.interface),
-    '0x3c7D0E3B1f82BF6a53a832c6E51bbc62B505EeDf'
+    process.env.FACTORYADDRESS
 );
 
 export default instance;
