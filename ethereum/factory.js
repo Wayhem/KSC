8912bafdc0a19303d131c9178fa2b07ff585d6d6
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-05 17:32:56 -0400

    ready for deployment

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import Factory from './build/Factory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(Factory.interface),
-    process.env.FACTORYADDRESS
+    process.env.FACTORYADDRESS || '0x858B3ECc344d1a326a429C2A24f41919789899b8'
 );
 
 export default instance;
